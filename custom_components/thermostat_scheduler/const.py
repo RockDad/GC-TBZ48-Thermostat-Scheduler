8fb5289f48373f9badc038c4cce58da86ef3cc9e
Joe Griffin <jgriffin716@gmail.com>
2025-09-18 13:16:39 -0400

    Initial add after file cleanup

---
+++ custom_components/thermostat_scheduler/const.py
@@ -0,0 +1,29 @@
+DOMAIN = "thermostat_scheduler"
+PLATFORMS = ["select", "number", "time"]
+
+# Device
+DEVICE_NAME = "Thermostat Scheduler"
+DEVICE_MANUFACTURER = "Custom"
+DEVICE_MODEL = "Thermostat Scheduler"
+DEVICE_ENTRY_TYPE = "service"
+
+# Temperature and Tolerance
+STEP_MINOR = 0.01
+STEP_MAJOR = 0.5
+DEFAULT_TOLERANCE = 1.0
+DEFAULT_TOLERANCE_MIN = 0.5
+DEFAULT_TOLERANCE_MAX_C = 5.0
+DEFAULT_TOLERANCE_MAX_F = 10
+
+# Time Settings
+DEFAULT_TIME_STRING = "06:00"
+
+# Sensor Setting
+DEFAULT_SENSOR = "sensor.none_found"
+SENSOR_TYPE_TEMPERATURE = "temperature"
+UNIT_CELSIUS = "°C"
+UNIT_FARENHEIT = "°F"
+
+# Thermostat state
+STATE_OPTIONS = ["idle", "heat", "cool"]
+STATE_DEFAULT = "idle"
